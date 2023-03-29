8e83071688c57d52449f1f3a38d48f05e1456c11
jofranko <jofranko@uninorte.edu.co>
2023-03-29 14:55:20 -0500

    second

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import "./styles/Profile.css";
+import photo from "../img/profile.jpeg";
+
 
 export default class Profile extends Component {
   render() {
     const { description, info } = this.props;
     return (
       <div className="perfil">
-        <img src="https://placeimg.com/192/192/people" alt="" />
+        <img src={photo} alt="Profile" />
         <span>{description}</span>
         <h2>{info}</h2>
       </div>
